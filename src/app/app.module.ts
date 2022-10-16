aa0191cfb00050bbf9e4c63f723cb018919dadc4
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-17 01:52:57 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule ,Routes } from '@angular/router';
 
+
+const appRoutes: Routes=[
+  {
+    path:"",component:HomeComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +21,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
